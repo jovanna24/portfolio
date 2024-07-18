0dd78bc2043ad0523e8ed7981bd3496d0e075dd5
jovanna24 <77941035+jovanna24@users.noreply.github.com>
2024-07-17 23:01:40 -0700

    fixing the email function

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -28,7 +28,7 @@ export const Contact = () => {
             let response = await fetch("http://localhost:5000/conatct", {
                 method: "POST",
                 headers: {
-                    "Content-Type": "application.json",
+                    "Content-Type": "application/json",
                 }, 
                 body: JSON.stringify(formDetails),
             }); 
